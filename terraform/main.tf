3a022525d2bc026b2f6f3eb8b752c4c7b3f24002
kingluxy <94976568+kingluxy@users.noreply.github.com>
2022-12-16 13:26:45 -0600

    control tower AFT test2

--- terraform/main.tf
+++ terraform/main.tf
@@ -25,3 +25,31 @@ module "sandbox" {
 
   account_customizations_name = "test1"
 }
+
+module "afttest2" {
+  source = "./modules/aft-account-request"
+
+  control_tower_parameters = {
+    AccountEmail              = "aftpoc2@gmail.com"
+    AccountName               = "aft-test2"
+    ManagedOrganizationalUnit = "AFT POC"
+    SSOUserEmail              = "saromibose@capstone-is.com"
+    SSOUserFirstName          = "Dec16"
+    SSOUserLastName           = "AFT"
+  }
+
+  account_tags = {
+    "Learn Tutorial" = "AFT"
+  }
+
+  change_management_parameters = {
+    change_requested_by = "Sam"
+    change_reason       = "AWS Control Tower Account Factory for Terraform"
+  }
+
+  custom_fields = {
+    group = "non-prod"
+  }
+
+  account_customizations_name = "test1"
+}
