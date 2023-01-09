589ead4e4fd64ac74ebcbf107af2c24bfb31e384
kingluxy <94976568+kingluxy@users.noreply.github.com>
2023-01-09 10:17:55 -0600

    jan9 review

--- terraform/main.tf
+++ terraform/main.tf
@@ -53,3 +53,31 @@ module "afttest2" {
 
   account_customizations_name = "test1"
 }
+
+module "jan9review" {
+  source = "./modules/aft-account-request"
+
+  control_tower_parameters = {
+    AccountEmail              = "jan9aftreview@gmail.com"
+    AccountName               = "jan9-review"
+    ManagedOrganizationalUnit = "AFT POC"
+    SSOUserEmail              = "saromibose@capstone-is.com"
+    SSOUserFirstName          = "JAN9"
+    SSOUserLastName           = "AFT"
+  }
+
+  account_tags = {
+    "Learn Tutorial" = "AFT"
+  }
+
+  change_management_parameters = {
+    change_requested_by = "Sam"
+    change_reason       = "AWS Control Tower Account Factory for Terraform"
+  }
+
+  custom_fields = {
+    group = "non-prod"
+  }
+
+  account_customizations_name = "test3"
+}
